ea167726b8003184fa63c491ec38cc9b60ce9a25
sysuKinthon <qinkai1994@qq.com>
2017-03-20 00:12:54 +0800

    java的接口使用

---
+++ java_basic/helloworld/src/EmployeeSortTest.java
@@ -0,0 +1,19 @@
+import EmployeeClass.*;
+
+import java.util.*;
+
+public class EmployeeSortTest
+{
+  public static void main(String[] args)
+  {
+    Employee[] staff = new Employee[3];
+
+    staff[0] = new Employee("harry hacker",  350000, 1989, 12, 1);
+    staff[1] = new Employee("harry hacker1",  250000, 1989, 12, 1);
+    staff[2] = new Employee("harry hacker2",  450000, 1989, 12, 1);
+
+    Arrays.sort(staff);
+    for (Employee e : staff)
+      System.out.print("name: " +  e.getName() + " , Salary= " + e.getSalary());
+  }
+}
