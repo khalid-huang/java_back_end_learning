f85ba43ae01bece64be3e64fcd50c02834de7bd1
sysuKinthon <qinkai1994@qq.com>
2017-03-23 13:24:36 +0800

    看了下example里面的内容

--- java_web_framework/java_servlet_jsp/run_tomcat.sh
+++ java_web_framework/java_servlet_jsp/run_tomcat.sh
@@ -1,5 +1,6 @@
-javac -d ./tomcat/webapps/ROOT/WEB-INF/classes ./tomcat/webapps/ROOT/WEB-INF/src/*.java
-echo "javac done; start tomcat catalia.sh"
+javac -d ./tomcat/webapps/ROOT/WEB-INF/classes ./tomcat/webapps/ROOT/WEB-INF/src/*.java && echo "java done; start tomcat catalia.sh" &&  ./tomcat/bin/catalina.sh run
+#echo "javac done; start tomcat catalia.sh"
+#echo "javac done; start tomcat catalia.sh"
 
 #./tomcat/bin/catalina.sh stop
-./tomcat/bin/catalina.sh run
+#./tomcat/bin/catalina.sh run
---
+++ java_web_framework/java_servlet_jsp/tomcat/webapps/ROOT/WEB-INF/src/PageRedirect.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/PageRedirect")
+public class PageRedirect extends HttpServlet
+{
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+              throws ServletException, IOException
+  {
+    response.setContentType("text/html; charset=UTF-8");
+
+    String site = new String("http://www.runoob.com");
+
+    response.setStatus(response.SC_MOVED_TEMPORARILY);
+    response.setHeader("Location", site);
+  }
+}
